8c60dec7d4f8b79f73dcb87a8177e2a6f50abe44
mango-days <77330707+mango-days@users.noreply.github.com>
2021-11-22 17:24:31 +0530

    Update top_down_merge_sort.py

--- top_down_merge_sort.py
+++ top_down_merge_sort.py
@@ -8,33 +8,34 @@ def re_arrange(array1, array2):
         if array1[index1]>array2[index2]:
             ans.append(array2[index2])
             index2+=1
+            if (index1==len(array1)):
+                while(index2!=len(array2)): 
+                    ans.append(array2[index2])
+                    index2+=1
+                break
         elif array1[index1]<array2[index2]:
             ans.append(array1[index1])
             index1+=1
+            if (index2==len(array2)): 
+                while(index1!=len(array1)): 
+                    ans.append(array1[index1])
+                    index1+=1
+                break
         else:   # array1[index1]==array2[index2]:
             ans.append(array1[index1])
             ans.append(array2[index2])
             index1+=1
             index2+=1
             
-        if (index1==len(array1)):
-            while(index2!=len(array2)): 
-                ans.append(array2[index2])
-                index2+=1
-        elif (index2==len(array2)): 
-            while(index1!=len(array1)): 
-                ans.append(array1[index1])
-                index1+=1
-                
         if (len(ans)==(len(array1)+len(array2))): break
     return(ans)
     
 def separate(array):
-    right_index=len(array)
-    left_index=1
     if (len(array)>1): 
-        middle_index=int(left_index + (right_index-1)/2)
-        ans=(re_arrange (separate(array[:middle_index]),separate(array[middle_index:])))
+        middle_index=int( len(array)/2 )
+        ans1=separate(array[:middle_index])
+        ans2=separate(array[middle_index:]
+        ans=re_arrange (ans1 ,ans2)
         return (ans)
     return (array)
     
